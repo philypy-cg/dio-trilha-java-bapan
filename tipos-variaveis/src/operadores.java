f381b77b280290a4e079f64c4367fe6351c51b2c
Philypy Gomes <phcg.dev@gmail.com>
2023-02-25 19:25:39 -0300

    resultado true and false

---
+++ tipos-variaveis/src/operadores.java
@@ -0,0 +1,19 @@
+public class operadores {
+    
+    public static void main(String[] args) {
+       int a, b;
+       a = 5;
+       b = 6;
+
+       String resultado ="";
+        if(a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+        
+       System.out.println(resultado); 
+            
+
+        
+    }
+}
